172700d22d5c55736dc70b7108a1b4b65e0b2605
DKB21 <88276913+DKB21@users.noreply.github.com>
2026-06-20 01:50:44 +0700

    fix: update gitignore

--- backend/dist/src/core/auth.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SECRET_KEY;
-if (!supabaseUrl || !supabaseKey) {
-    throw new Error("Missing env variables");
-}
-export const auth = createClient(supabaseUrl, supabaseKey).auth;
--- backend/dist/src/module/users/dtos.js
+++
@@ -1,4 +0,0 @@
-import { z } from "zod";
-export const CreateUserRequestSchema = z.object({
-    name: z.string().min(1).max(100),
-});
